ac706933178f1b267c256a517ff621d7c5bd77c9
Lyuda Barabash <barabashlyuda@gmail.com>
2018-08-27 20:47:26 +0200

    Display total price

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ShoppingCart.css';
 
 import Heading from '../Heading/Heading.js';
 import Orders from './Orders/Orders.js';
 import TotalPrice from './TotalPrise/TotalPrise.js';
 
-const shoppingCart = (props) => {
-  return (
-    <div className="shoppingCart">
-      <Heading header={'Shoping cart'} />
-      <Orders
-        addedProducts={props.selectedProducts}
-        sendProductName={props.activateDelete}
-      />
-      <TotalPrice />
-    </div>
-  );
+class ShoppingCart extends Component {
+  state = {
+    totalPrice: 0
+  }
+
+  // updateTotal = (amount = 0) => {
+  //   const total = {...this.state};
+  //   const updatedTotal = total.totalPrice + amount;
+  //   this.setState({ totalPrice: updatedTotal })
+  // }
+
+  render() {
+    return (
+      <div className="shoppingCart">
+        <Heading header={'Shoping cart'} />
+        <Orders
+          addedProducts={this.props.selectedProducts}
+          sendProductName={this.props.activateDelete}
+        />
+        <TotalPrice
+          totalPrice={this.state.totalPrice}
+        />
+      </div>
+    );
+  }
 }
 
-export default shoppingCart;
+export default ShoppingCart;
--- src/components/ShoppingCart/TotalPrise/TotalPrise.js
+++ src/components/ShoppingCart/TotalPrise/TotalPrise.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './TotalPrise.css';
 
+const displayTotal = (amount) => {
+  if (amount <= 0) return 0;
+  return amount;
+}
+
 const totalPrice = (props) => {
+
   return (
     <div className="totalPrice">
       <div>Total</div>
-      <div>{`25 EUR`}</div>
+      <div>{`${displayTotal(props.totalPrice)} EUR`}</div>
     </div>
   );
 }
